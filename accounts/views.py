5cd94c4d7e6cdda2b0537ffccf0d3c7c6c02a030
xxddongxx <hyeon2187@gmail.com>
2022-11-03 22:01:50 +0900

    FEAT: 가계부 작성 api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from accounts import serializers
+
+
+class Accounts(APIView):
+    def post(self, request):
+        """
+        가계부 작성
+        """
+        serializer = serializers.AccountsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/users/", include("users.urls")),
-
+    path("api/v1/accounts/", include("accounts.urls")),
 ]
 
 # 토큰
