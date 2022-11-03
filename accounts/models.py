b21b51aec56decce98e9f8f68b247d6f2fb10246
xxddongxx <hyeon2187@gmail.com>
2022-11-03 21:46:22 +0900

    FEAT: 가계부 모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    amount = models.PositiveIntegerField(verbose_name="금액")
+    memo = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="작성시간")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정시간")
+
+    def __str__(self):
+        return f'{self.amount}: {self.memo[:20]}'
