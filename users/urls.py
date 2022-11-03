223bcf7a131f729cafea3c5258fc9194fef95eca
xxddongxx <hyeon2187@gmail.com>
2022-11-04 03:08:39 +0900

    FEAT: token login

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users import views
 
 urlpatterns = [
     path("", views.UsersView.as_view()),
     path("register/", views.UsersRegister.as_view()),
-    path("login/", views.Login.as_view()),
+    path("login/", TokenObtainPairView.as_view()),
     path("logout/", views.Logout.as_view()),
-    path("<int:pk>/", views.UsersDetail.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+    # path("<int:pk>/", views.UsersDetail.as_view()),
 ]
