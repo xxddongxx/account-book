4e86338e967a5f73a6f978542f0b4f534cdb5b24
xxddongxx <hyeon2187@gmail.com>
2022-11-04 18:02:12 +0900

    FEAT: User 생성 테스트

--- users/tests.py
+++ users/tests.py
@@ -3,40 +3,27 @@ from rest_framework import status, response
 from rest_framework.test import APITestCase, APIClient
 from rest_framework_simplejwt.tokens import Token
 
+from accounts.models import Account
 from users.models import User
 
 
 class AccountTests(APITestCase):
-    def test_create_account(self):
+    register_url = "/api/v1/users/"
+
+    def test_register(self):
         """
-        Ensure we can create a new account object.
+        회원가입 성공 테스트
         """
+
         data = {"username": "test@test.com", "password": "testpw"}
-        user = User.objects.create_user(username="test@test.com", password="testpw")
-        print("===================user=================", user.username)
-        response = self.client.post("/api/v1/users/login/", data=data, format="json")
+        response = self.client.post(self.register_url, data, format("json"))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.get().username, "test@test.com")
 
-        refresh = response.data.get("refresh")
-        access = response.data.get("access")
-        refresh_response = self.client.post(
-            "/api/v1/users/login/", data={"refresh": refresh}, format="json"
-        )
-        print("===============refresh_response==============", refresh_response)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Log out
-        self.client.logout()
-
-        # url = reverse("register")
-        # print("url >>> ", url)
-        #
-        # data = {"username": "test@test.com", "password": "testpw"}
-        # response = self.client.post(url, data, format="json")
-        # print("response >>> ", response)
-        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # self.assertEqual(User.objects.count(), 1)
-        # self.assertEqual(User.objects.get().username, "test@test.com")
-
-        # Include an appropriate `Authorization:` header on all requests.
-        # token = Token.objects.get(user__username="lauren")
-        # client = APIClient()
-        # client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+    def test_register_fail(self):
+        """
+        회원가입 실페 테스트
+        """
+        data = {"username": "test@test.com"}
+        response = self.client.post(self.register_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
